cd0a7982bdc8f18588df96fba251c089175e1be5
Yulia Zamriy <yulia.zamriy@gmail.com>
2017-09-05 13:02:12 -0400

    made code more agile

--- Pulling_USAPL_parameters.py
+++ Pulling_USAPL_parameters.py
@@ -1,66 +1,76 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Aug 17 17:44:10 2017
 
 @author: yzamriy
 
-Pulling parameters of ranking database
-Output: nested dictionary called usapl_parameters
+Goal:   extrat USAPL structure parameters (divisions, levels, etc) 
+        These parameters will be used to extract specific data from 
+        the database (about lifters and competition)
+
+Output: 1. Nested dictionary called usapl_parameters 
+        2. .txt file 
 
 """
 
 from bs4 import BeautifulSoup
-import urllib.request
+import urllib3
 import sys
 
+http = urllib3.PoolManager()
 url = 'http://usapl.liftingdatabase.com/rankings'
-req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-r = urllib.request.urlopen(req).read()
-soup = BeautifulSoup(r, "lxml")
+response = http.request('GET', url)
+soup = BeautifulSoup(response.data, "lxml")
 
-top_rows = soup.find_all("tr")[:7]
+# all the necessary data is contained in the top part of the page
+top_rows = soup.find_all("tr", limit = 7)
+
+# usapl_parameters is a nested dictionary
+# First layer contains the following keys:
+# Sex, Division, Weightclass, Exercise, State, Year, Order by
+# Second layer exists only for Weightclass. The keys are
+# IPF - Female; IPF - Male; USAPL Nationals - Female; USAPL Nationals - Male
+# The last two are old weight classes
 
 usapl_parameters = {}
 for p in top_rows:
-    usapl_parameters[p.contents[1].get_text()] = {}
-    
-for p in top_rows:
-    l = p.contents[3].get_text().strip('\n').split('\n')
-    v = p.contents[3].find_all("option")
-    for i, j in zip(l,v):
-        usapl_parameters[p.contents[1].get_text()][i] = j["value"]
+    key = p.find("th").get_text()
+    usapl_parameters[key] = {}
+    if key == "Weightclass":
+        weightclasses = {}
+        for fed in p.find_all("optgroup"):
+            weightclasses[fed["label"]] = {}
+            wclass = fed.find_all("option")
+            for w in wclass:
+                weightclasses[fed["label"]][w.get_text()] = w["value"]
+        usapl_parameters["Weightclass"] = weightclasses
+    else:    
+        for option in p.find_all("option"):
+            code = option["value"]
+            textname = option.get_text()
+            usapl_parameters[key][textname] = code
 
-""" Weight classes are nested within different categories
-    USAPL Nationals are old weight classes
-    IPF are new classes
-"""
-
-fed = top_rows[2].contents[3].find_all("optgroup")
-wclass = top_rows[2].contents[3].find_all("option")    
-usapl_parameters["Weightclass"] = {}
-weightclasses = {}
-
-fed = top_rows[2].contents[3].find_all("optgroup")
-for f in fed:
-    weightclasses[f["label"]] = {}
-    wclass = f.find_all("option")
-    for w in wclass:
-        weightclasses[f["label"]][w.get_text()] = w["value"]
-
-usapl_parameters["Weightclass"] = weightclasses
-
+# Write the dictionary to a txt file
 orig_stdout = sys.stdout
-f = open("usapl_parameters.txt", 'w')
-sys.stdout = f
+file = open("usapl_parameters.txt", 'w')
+sys.stdout = file
 
 for i in usapl_parameters:
-    print("Parameter:", i)
-    print()
-    print("Possible values:")
-    for j in usapl_parameters[i].keys():
-        print(j)
     print()
     print("-"*2)
+    print("Parameter:", i)
+    if i == "Weightclass":
+        for fed in usapl_parameters[i]:
+            print()
+            print("Federation:", fed)
+            print("Possible values:")
+            for w in usapl_parameters[i][fed]:
+                print(w)
+    else:
+        print()
+        print("Possible values:")
+        for j in usapl_parameters[i].keys():
+            print(j)
         
+file.close()
 sys.stdout = orig_stdout
-f.close()
+
