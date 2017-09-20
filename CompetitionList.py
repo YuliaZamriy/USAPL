52b26d58adac0ef1007b0aa97213bdffb1aa0fa7
Yulia Zamriy <yulia.zamriy@gmail.com>
2017-09-20 19:41:38 -0400

    changed the definition
    made the code more consistent

--- CompetitionList.py
+++ CompetitionList.py
@@ -4,97 +4,106 @@ Created on Tue Sep 12 14:48:34 2017
 
 @author: yzamriy
 
-Goal:   Create object CompetitionList that contains the list of all 
-        available USAPL competitions
-        1. Scrape the names of all the available competitions from USAPL site
-        2. Put competition names into a nested dictionary for access by other
-        classes
+Goal:   Create object CompetitionList that contains a dictionary with all 
+        available USAPL competitions as keys
+        It inherets all attributes and methods from UsaplData object
         
-Inputs: Optional:   'level' is 'NS' for national and 'RG' for regional
-                    'sub' any substring within the name of the competition.
-                    For example, 'Raw' or 'High School'
+Inputs (Optional):   
+        'level' is 'NS' for national and 'RG' for regional
+        'sub' any substring within the name of the competition.
+        For example, 'Raw' or 'High School'
 
-Requirements: Soup.py to get Beautiful Soup objects
+Main methods: 1. get_comp_names: creates a dictionary with 
+                 key = competition url reference
+                 value = competition name
+              2. comp_names_to_txt: prints the output dictionary from
+                 get_comp_names to a .txt file
+              3. get_comp_ref: based on competition name, return its url reference
+              4. get_comp_date: based on competition reference, return 
+                 competition date
+              4. get_comp_name: based on competition reference, return 
+                 competition name
+    
+Requirements: UsaplData.py to get UsaplData object
 """
 
-from Soup import Soup
+from UsaplData import UsaplData
 import sys
-import datetime
 
-
-class CompetitionList(object):
-    def __init__(self):
+class CompetitionList(UsaplData):
+    def __init__(self, level = '', substring = ''):
         '''
         Goal:       Initializes CompetitionList object
-        Details:    Soup object has one constant attribute:
-                        self.report_type = 'competitions'
-        Returns:    Nothing   
+        Details:    CompetitionList object has following attributes:
+                    inherited:
+                        self.data_dict: nested dictionary with data
+                        self.col_names: list with key names from the dictionary
+                    own:
+                        self.reference: string. Part of the url address for target page
+                        self.level: input string, indicated competition level
+                        self.substring: input string, used to subset comptitions
+                        self.comp_names: nested dictionary containing 
+                                         competition names and url references
         '''
-        self.report_type = 'competitions'
+        self.reference = 'competitions'
+        UsaplData.__init__(self, self.reference)
+        self.level = level
+        self.substring = substring
+        self.comp_names = self.get_comp_names()
 
-    def build_comp_dict(self):
-        ''' 
-        Goal:    Build nested dictionary with
-                     keys => competition names,
-                     values => competition details (time, sanction # etc.)
-        Returns:    List of available competitions with details, nested dictionary   
-        '''    
-        comp_soup = Soup(self.report_type).get_soup()
-        comp_soup_colnames = comp_soup.find("table", class_="tabledata").find('thead').find_all('th')
-        comp_soup_table = comp_soup.find("table", class_="tabledata").find('tbody').find_all('tr')
-        
-        self.usapl_comps = {}
-        for comp in comp_soup_table:
-            comp_name = comp.find('a').get_text().strip()
-            self.usapl_comps[comp_name] = {}
-            for cl, cv in zip(comp_soup_colnames, comp.find_all('td')):
-                col_names = cl.get_text()
-                col_value = cv.get_text().strip()
-                self.usapl_comps[comp_name][col_names] = col_value
-            self.usapl_comps[comp_name]["link"] = comp.find('a')["href"]
-       
-        return self.usapl_comps 
-
-    def get_comp_names(self, level = '', sub = ''):
+    def get_comp_names(self):
         ''' 
         Goal:    Get the list of competition names satisfying input criteria
-        Input:   Optional:
-                     'level': Competition level (National, Regional etc.), string
-                     'sub':   Substring in the required competition name, string
-        Returns:    Competition names, list   
+                 based on self.level and self.substring values
+        Returns: nexted dictionary with a subset of competition names
         '''    
-        comp_dict = self.build_comp_dict()
-        self.comp_names = []
-        for comp in comp_dict:
-            if comp_dict[comp]['Sanction #'].find(level) >= 0 and comp_dict[comp]['Name'].find(sub) >= 0:
-                self.comp_names.append(comp_dict[comp]['Name'])
-        return self.comp_names
+        comp_names = {}
+        for comp in self.data_dict:
+            if self.data_dict[comp]['Sanction #'].find(self.level) >= 0 and self.data_dict[comp]['Name'].find(self.substring) >= 0:
+                comp_names[comp] = self.data_dict[comp]['Name']
+        return comp_names
 
-    def comp_names_to_txt(self, level = '', sub = ''):
+    def comp_names_to_txt(self):
         ''' 
-        Goal:    Print the list of competition names satisfying input criteria
-        Input:   Optional:
-                     'level': Competition level (National, Regional etc.), string
-                     'sub':   Substring in the required competition name, string
-        Output:  .txt file with all the names in the same directory
+        Goal:    Print the list of competitions satisfying input criteria
+        Output:  .txt file with all the names and url references in the same directory
         Returns:  Nothing   
         '''    
-        pull_date = datetime.datetime.today().strftime("%m%d%Y")
-        filename = "usapl_comps_"+level+sub+"_"+pull_date+".txt"
-        usapl_comps = self.get_comp_names(level, sub)
-        
+        filename = "usapl_comps"+self.level+self.substring+".txt"
         orig_stdout = sys.stdout
         file = open(filename, 'w')
         sys.stdout = file
         
-        if level != '':
-            print('Sanction:', level, sub)
+        if self.level != '':
+            print('Sanction:', self.level, self.substring)
             print('-'*20)
-        for i in usapl_comps:
-            print(i.encode("utf-8"))
+        for ref in self.comp_names:
+            print(self.comp_names[ref].encode("utf-8"), "link ref:", ref)
                 
         file.close()
         sys.stdout = orig_stdout
 
-#r = CompetitionList()
-#r.comp_names_to_txt(level = '', sub = '')
+    def get_comp_ref(self, comp_name):
+        '''
+        Returns: list contaiing competition url reference based on competition name
+                 There might be multiple competition names with the same name
+                 Hence, multiple references in the list are possible
+        '''
+        ref_list = []
+        for ref in self.comp_names:
+            if self.comp_names[ref] == comp_name:
+                ref_list.append(ref)
+        return ref_list
+
+    def get_comp_date(self, comp_ref):
+        '''
+        Returns: date for the competition specified by the url reference
+        '''
+        return self.data_dict[comp_ref]['Date']
+
+    def get_comp_name(self, comp_ref):
+        '''
+        Returns: competition name specified by the url reference
+        '''
+        return self.data_dict[comp_ref]['Name']
+
